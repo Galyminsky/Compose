ebbd50e801d13e204af15ba2b3a33d46e9c8e634
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 16:49:43 +0600

    #7

--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.galyaminsky.compose
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+        ListItem(name = "Lisander", prof = "Developer")
         }
     }
 }
@@ -31,7 +43,23 @@ private fun ListItem(name: String, prof: String) {
         elevation = CardDefaults.cardElevation(5.dp)
 
     ) {
-
+    Box {
+      Row (verticalAlignment = CenterVertically) {
+          Image(
+              painter = painterResource(id = R.drawable.ava),
+              contentDescription = "image",
+              contentScale = ContentScale.Crop,
+              modifier = Modifier
+                  .padding(10.dp)
+                  .size(64.dp)
+                  .clip(CircleShape)
+          )
+          Column {
+              Text(text = name)
+              Text(text = prof)
+          }
+      }
+    }
     }
 }
 

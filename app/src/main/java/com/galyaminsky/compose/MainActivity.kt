33d6f2b5ceedbadaea76adeef30444f2243541a6
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 16:31:18 +0600

    #6

--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -3,22 +3,14 @@ package com.galyaminsky.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.galyaminsky.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,3 +20,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+
+    ) {
+
+    }
+}
+

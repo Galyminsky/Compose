97cdc02a5c46d6105d171032ab316f0c24d976e0
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 17:57:25 +0600

    #12

--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,6 +21,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,14 +55,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun ListItem(name: String, prof: String) {
+    var counter = remember {
+        mutableStateOf(0)
+    }
     Card(
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
-            .pointerInput(Unit) {
-                detectTapGestures {
-                    Log.d("MyLog", "ListItem: $it")
-                }
+            .clickable {
+                counter.value++
             },
         shape = RoundedCornerShape(10.dp),
         elevation = CardDefaults.cardElevation(5.dp)
@@ -81,7 +85,7 @@ private fun ListItem(name: String, prof: String) {
                         .padding(start = 10.dp)
 
                 ) {
-                    Text(name)
+                    Text(counter.value.toString())
                     Text(prof)
                 }
             }

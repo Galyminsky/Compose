219f8c83250a209e32443626753e806ffbbdb873
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 15:54:42 +0600

    #3

--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -3,12 +3,19 @@ package com.galyaminsky.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.galyaminsky.compose.ui.theme.ComposeTheme
 
@@ -16,13 +23,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Row(
+                modifier = Modifier
+                    .background(Color.Green).fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Lisander")
+                Text(text = "Galyaminsky")
+                Text(text = "Yurik")
+            }
         }
     }
-}
-
-@Preview (showBackground = true)
-@Composable
-fun NewText() {
-    Text(text = "Lisander 54")
 }

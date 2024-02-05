feaea5bfe0a13071e493d343930096d4c38bf27c
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 18:34:00 +0600

    #13

--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -1,95 +1,67 @@
 package com.galyaminsky.compose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
-                ListItem(name = "Lisander", prof = "Developer")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Center
+            ) {
+                CircleItem()
             }
         }
     }
 }
 
 @Composable
-private fun ListItem(name: String, prof: String) {
-    var counter = remember {
+private fun CircleItem() {
+
+    val counter = remember {
         mutableStateOf(0)
     }
-    Card(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth()
-            .clickable {
-                counter.value++
-            },
-        shape = RoundedCornerShape(10.dp),
-        elevation = CardDefaults.cardElevation(5.dp)
-    ) {
-        Box {
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.ava),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(64.dp)
-                        .padding(10.dp)
-                        .clip(CircleShape)
-                )
-                Column(
-                    modifier = Modifier
-                        .align(CenterVertically)
-                        .padding(start = 10.dp)
-
-                ) {
-                    Text(counter.value.toString())
-                    Text(prof)
-                }
-            }
-        }
+    val color = remember {
+        mutableStateOf(Color.Blue)
     }
+    Box(
+        modifier = Modifier
+            .size(150.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+                when (++counter.value) {
+                    10 -> color.value = Color.Red
+                    20 -> color.value = Color.Green
+                    30 -> color.value = Color.Blue
+                }
+
+            }, contentAlignment = Alignment.Center
+    ) {
+
+    }
+    Text(
+        text = counter.value.toString(),
+        style = TextStyle(color = Color.Yellow, fontSize = 50.sp)
+    )
 }
 

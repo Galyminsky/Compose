eac7eba7acbc1bc487783d4171c072e0249415aa
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-05 23:18:40 +0600

    #18

---
+++ app/src/main/java/com/galyaminsky/compose/ItemRowModel.kt
@@ -0,0 +1,6 @@
+package com.galyaminsky.compose
+
+data class ItemRowModel(
+    val imageId: Int,
+    val title: String
+)
--- app/src/main/java/com/galyaminsky/compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/compose/MainActivity.kt
@@ -3,35 +3,38 @@ package com.galyaminsky.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.galyaminsky.compose.ui.theme.Blue100
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LazyRow(
-                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
-                    .fillMaxSize(),
+                    .fillMaxWidth()
+                    .background(Blue100),
 
                 ) {
                 itemsIndexed(
-                    listOf("item1", "item2", "item3", "item4", "item5", "item6", "item7")
-                ) {index, item ->
-                    Text(
-                        text = item,
-                        fontSize = 30.sp,
-                        modifier = Modifier.padding(horizontal = 10.dp)
+                    listOf(
+                        ItemRowModel(R.drawable.ava, "Lisander"),
+                        ItemRowModel(R.drawable.ava1, "Oleg"),
+                        ItemRowModel(R.drawable.ava2, "Baukmod"),
+                        ItemRowModel(R.drawable.ava3, "Alex"),
+                        ItemRowModel(R.drawable.ava4, "Sahsa"),
+                        ItemRowModel(R.drawable.ava5, "Misha"),
+                        ItemRowModel(R.drawable.ava6, "Tatyana"),
+                        ItemRowModel(R.drawable.ava7, "Ivan"),
+                        ItemRowModel(R.drawable.ava9, "Maks"),
+                        ItemRowModel(R.drawable.ava10, "Andrew"),
                     )
+                ) { index, item ->
+                    MyRow(item = item)
                 }
             }
         }
